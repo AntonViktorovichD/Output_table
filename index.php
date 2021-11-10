0e4753929399cca9b283fd6756b99f1e8ba3ec58
Anton Viktorovich <75506917+AntonViktorovichD@users.noreply.github.com>
2021-11-10 10:24:15 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -36,111 +36,133 @@ foreach ($arrExist as $row) {
 }
 
 if (in_array($polis, $arrPolis)) {
-   echo $polis . " найден";
-   $query = "SELECT POLIS, V_POS, SUM_POS, MES_N, YEAR(MES_N) AS YEAR, MONTH(MES_N) AS MONTH FROM TEST_TABLE WHERE POLIS ='" . $polis . "' ORDER BY V_POS, MES_N";
+echo $polis . " найден";
+$query = "SELECT POLIS, V_POS, SUM_POS, MES_N, YEAR(MES_N) AS YEAR, MONTH(MES_N) AS MONTH FROM TEST_TABLE WHERE POLIS ='" . $polis . "' ORDER BY V_POS, MES_N";
 
-   $result = mysqli_query($link, $query) or die(mysqli_error($link));
-   $i = 0;
-   for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row) ;
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+$i = 0;
+for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row) ;
 
-   $year_tmp = '';
-   $year = array();
-   foreach ($data as $el) {
-      $year[] .= $el['YEAR'];
-      $year = array_unique($year);
-      asort($year);
-   }
-   ?>
-    <table>
-        <tr>
-            <th rowspan="2">POLIS</th>
-            <th rowspan="2">V_POS</th>
-           <?php
-           foreach ($year as $yearStr)
-              echo '<th rowspan="1" colspan="12">' . $yearStr . '</th>';
-           ?>
-        </tr>
-        <tr>
-           <?
-           $monthNum = '';
-           $month = array();
-           for ($monthNum = 1; $monthNum < 13; $monthNum++) {
-              if ($monthNum > 0 && $monthNum < 10) {
-                 $monthNum = '0' . $monthNum;
-                 echo '<td>' . $monthNum . '</td>';
-              } else {
-                 echo '<td>' . $monthNum . '</td>';
-              }
-              $month[] .= $monthNum;
-           }
-           ?>
-        </tr>
-       <?php
-       $revpos = 0;
-       $revvpos = 0;
-       $arr = array_fill_keys($month, "-");
-       $len = count($data);
-       foreach ($data as $elem): $i++; ?>
-          <?php if ($revpos == 0) {
-             $revpos = $elem['POLIS'];
-             $revvpos = $elem['V_POS'];
-             foreach ($month as $val) {
-                if (date("m", strtotime($elem['MES_N'])) == $val) {
-                   $arr[$val] = $elem['SUM_POS'];
-                }
-             }
-          } else {
-             if ($revpos == $elem['POLIS'] && $revvpos == $elem['V_POS']) {
-                foreach ($month as $val) {
-                   if (date("m", strtotime($elem['MES_N'])) == $val) {
-                      $arr[$val] = $elem['SUM_POS'];
-                   }
-                }
-             } else {
-                ?>
-                 <tr>
-                     <td id="rs"><?= $revpos ?></td>
-                     <td><?= $revvpos ?></td>
-                    <?
-                    foreach ($month as $val) {
-                       echo '<td>' . $arr[$val] . '</td>';
-                    }
-                    ?>
-                 </tr>
-                <?php
-                $revpos = $elem['POLIS'];
-                $revvpos = $elem['V_POS'];
-                $arr = array_fill_keys($month, "-");
-                foreach ($month as $val) {
-                   if (date("m", strtotime($elem['MES_N'])) == $val) {
-                      $arr[$val] = $elem['SUM_POS'];
-                   }
-                }
-             }
-          }
-          if ($i == $len) {
-             ?>
-              <tr>
-                  <td id="disp"><?= $revpos ?></td>
-                  <td><?= $revvpos ?></td>
-                 <?
-                 foreach ($month as $val) {
-                    echo '<td>' . $arr[$val] . '</td>';
-                 }
-                 ?>
-              </tr>
-             <?php
-          }
-       endforeach; ?>
-    </table>
-<?php
+//   $year_tmp = '';
+//   $year = array();
+//   foreach ($data as $el) {
+//      $year[] .= $el['YEAR'];
+//      $year = array_unique($year);
+//      asort($year);
+//   }
 
+//   $year_tmp = '';
+//   foreach ($data as $el) {
+//      if ($year_tmp == '') {
+//      } elseif ($year_tmp == $el['YEAR']) {
+//         print("+");
+//      } else {
+//         print("-");
+//      }
+//      $year_tmp = $el['YEAR'];
+//   }
+$main_arr = array();
+$v_pos_arr = array();
+$year_arr = array();
 
-} else {
-   echo $polis . " не найден";
+foreach ($data as $v_pos) {
+   $v_pos_arr[] = $v_pos['V_POS'];
+}
+
+foreach ($data as $year) {
+   $year_arr[] = $year['YEAR'];
 }
 ?>
+<table>
+    <tr>
+        <th rowspan="2">POLIS</th>
+        <th rowspan="2">V_POS</th>
+       <?php
+       $v_pos_arr = array_unique($v_pos_arr);
+       $year_arr = array_unique($year_arr);
+       asort($year_arr);
+       foreach ($year_arr as $yearStr) {
+          echo '<th rowspan="1" colspan="12">' . $yearStr . '</th>';
+       } ?>
+    </tr>
+    <tr>
+       <?php
+       $monthNum = '';
+       $month = array();
+       foreach ($year_arr as $item) {
+          for ($monthNum = 1; $monthNum < 13; $monthNum++) {
+                echo '<td>' . $monthNum . '</td>';
 
+             $month[] = $monthNum;
+          }
+       }
+       $month = array_unique($month);
+       ?>
+    </tr>
+   <?php
+   $revpos = 0;
+   $revvpos = 0;
+   $arr = array_fill_keys($month, "-");
+   $len = count($data);
+   foreach ($data as $elem): $i++;
+      $main_arr[$elem['V_POS']][$elem['YEAR']][$elem['MONTH']] = $elem['SUM_POS'];
+      ?>
+      <?php if ($revpos == 0) {
+         $revpos = $elem['POLIS'];
+         $revvpos = $elem['V_POS'];
+         foreach ($month as $val) {
+            if (date("m", strtotime($elem['MES_N'])) == $val) {
+               $arr[$val] = $elem['SUM_POS'];
+            }
+         }
+      } else {
+         if ($revpos == $elem['POLIS'] && $revvpos == $elem['V_POS']) {
+            foreach ($month as $val) {
+               if (date("m", strtotime($elem['MES_N'])) == $val) {
+                  $arr[$val] = $elem['SUM_POS'];
+               }
+            }
+         } else {
+            ?>
+            <?php
+            $revpos = $elem['POLIS'];
+            $revvpos = $elem['V_POS'];
+            $arr = array_fill_keys($month, "-");
+            foreach ($month as $val) {
+               if (date("m", strtotime($elem['MES_N'])) == $val) {
+                  $arr[$val] = $elem['SUM_POS'];
+               }
+            }
+         }
+      }
+   endforeach; ?>
+
+   <?php
+   foreach ($v_pos_arr as $item) { ?>
+       <tr>
+           <td>
+              <?= $polis ?>
+           </td>
+           <td>
+              <?= $item ?>
+           </td>
+          <?php foreach ($year_arr as $year) {
+             foreach ($month as $mt) {
+                if (isset($main_arr[$item][$year][$mt])) {
+                   echo '<td>' . $main_arr[$item][$year][$mt] . '</td>';
+                } else {
+                   echo '<td> - </td>';
+                }
+             }
+          } ?>
+       </tr>
+      <?php
+   }
+   } else {
+      echo $polis . " не найден";
+   }
+   ?>
+</table>
 </body>
 <style>
     table {
